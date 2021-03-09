2492c6112aca8a4846cab54e32086bada9987613
William Quintal <wquintal@alborea.com>
2021-03-08 23:23:41 -0500

    pwa

--- projects/pwa/src/lib/pwa/config.ts
+++ projects/pwa/src/lib/pwa/config.ts
@@ -1,9 +1,14 @@
+import { InjectionToken } from '@angular/core';
 
+export interface PWAConfig {
+    autoUpdate?: boolean;
+}
 
+export const PWA_CONFIG = new InjectionToken<PWAConfig>('pwa.config');
 
 export const pwaConfig = {
-  serviceWorker: {
-    enabled: false,
-    path: 'ngsw-config.json'
-  }
+    serviceWorker: {
+        enabled: false,
+        path: 'ngsw-config.json',
+    },
 };
--- projects/pwa/src/lib/pwa/update.service.ts
+++ projects/pwa/src/lib/pwa/update.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
 import { NotificationService } from '@berlingoqc/ngx-notification';
+import { PWAConfig, PWA_CONFIG } from './config';
 
 @Injectable()
 export class AppUpdateService {
     availableUpdate: UpdateAvailableEvent;
 
     constructor(
+        @Inject(PWA_CONFIG) private config: PWAConfig,
         private readonly updates: SwUpdate,
         private notificationService: NotificationService,
     ) {
         if (this.updates.isEnabled) {
             this.updates.available.subscribe((event) => {
+                if (this.config.autoUpdate) {
+                    this.executeUpdate();
+                    return;
+                }
                 this.availableUpdate = event;
                 this.notificationService.openNotification({
                     title: 'Nouvelle mise à jour disponible',
