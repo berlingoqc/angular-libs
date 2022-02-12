4b19fd4f8ac311e87ce1f607259aa43c4925881f
William Quintal <wquintal@alborea.com>
2022-02-11 21:09:02 -0500

    version(2.2.8)
    * action confirmation add service with rxjs pipe
    
    * add div around table to allow scrolling

--- projects/common/src/lib/confirmation/index.ts
+++ projects/common/src/lib/confirmation/index.ts
@@ -1,4 +1,3 @@
-import { from } from 'rxjs';
-
+export * from './action-confirmation.service';
 export * from './action-confirmation-dialog/action-confirmation-dialog.component';
 export * from './confirmation.module';
